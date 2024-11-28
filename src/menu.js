660e5ab6dfa6244b89c1f6821acb5341bca12af1
anna-rurak <anna.rurak@gmail.com>
2024-11-28 18:27:40 +0100

    adding menu object file

---
+++ src/menu.js
@@ -0,0 +1,141 @@
+const menu = [
+    {
+        id: 1,
+        name: "Bruschetta Pomodoro",
+        price: 20,
+        description:
+            "grillowane grzanki z oliwą z oliwek, pomidorami, bazylią i czosnkiem",
+        type: "appetizer",
+    },
+    {
+        id: 2,
+        name: "Caprese",
+        price: 25,
+        description:
+            "sałatka z dojrzałych pomidorów, mozzarelli di bufala, śweżej bazylii i oliwy",
+        type: "appetizer",
+    },
+    {
+        id: 3,
+        name: "Aranchini",
+        price: 15,
+        description:
+            "smażone kulki ryżowe nadziewane serem i sosem pomidorowym",
+        type: "appetizer",
+    },
+    {
+        id: 4,
+        name: "Minestrone",
+        price: 17,
+        description: "klasyczna włoska zupa z dodatkiem makaronu",
+        type: "soup",
+    },
+    {
+        id: 5,
+        name: "Zuppa di Pomodoro",
+        price: 18,
+        description: "gęsta zupa pomidorowa z bazylią i kremowym mascarpone",
+        type: "soup",
+    },
+    {
+        id: 6,
+        name: "Spaghetti Carbonara",
+        price: 33,
+        description:
+            "makaron z kremowym sosem na bazie jajek, huanciale i sera pecorino",
+        type: "main-course",
+    },
+    {
+        id: 7,
+        name: "Lasagna Bolognese",
+        price: 34,
+        description:
+            "warstwy makaronu z sosem bolońskim, beszamelowym i mozzarellą",
+        type: "main-course",
+    },
+    {
+        id: 8,
+        name: "Gnocchi al Pesto",
+        price: 28,
+        description:
+            "miękkie kluseczki ziemniaczane z aromatycznym besto z bazylii i orzeszkami piniowymi",
+        type: "main-course",
+    },
+    {
+        id: 9,
+        name: "Risotto alla Milanese",
+        price: 30,
+        description: "szafranowe risotto z odrobiną parmezanu",
+        type: "main-course",
+    },
+    {
+        id: 10,
+        name: "Margherita",
+        price: 25,
+        description:
+            "sos pomidorowy, mozarella, świeża bazylia, oliwa z oliwek",
+        type: "pizza",
+    },
+    {
+        id: 11,
+        name: "Marinara",
+        price: 25,
+        description:
+            "sos pomidorowy, czosnek, oregano, oliwa z oliwek (bez sera)",
+        type: "pizza",
+    },
+    {
+        id: 12,
+        name: "Quatro Formaggi",
+        price: 30,
+        description:
+            "mozarella, gorgonzola, parmezan, ser kozi, oliwa z oliwek",
+        type: "pizza",
+    },
+    {
+        id: 13,
+        name: "Pepperoni",
+        price: 35,
+        description: "sos pomidorowy, mozarella, pikantne pepperoni, oregano",
+        type: "pizza",
+    },
+    {
+        id: 14,
+        name: "Diavola",
+        price: 36,
+        description:
+            "sos pomidorowy, mozarella, pikantne pepperoni, papryczki chilli",
+        type: "pizza",
+    },
+    {
+        id: 15,
+        name: "Prosciutto e Funghi",
+        price: 36,
+        description: "sos pomidorowy, mozarella, szynka parmeńska, pieczarki",
+        type: "pizza",
+    },
+    {
+        id: 16,
+        name: "Capriciosa",
+        price: 34,
+        description:
+            "sos pomidorowy, mozarella, szynka, karczochy, pieczarki, oliwki",
+        type: "pizza",
+    },
+    {
+        id: 17,
+        name: "Frutti di Mare",
+        price: 43,
+        description:
+            "sos pomidorowy, mozarella, krewetki, małże, kalmary, czosnek",
+        type: "pizza",
+    },
+    {
+        id: 18,
+        name: "Vegetariana",
+        price: 28,
+        description:
+            "sos pomidorowy, mozarella, grillowana cukinia, bakłażan, papryka, pieczarki",
+        type: "pizza",
+    },
+];
