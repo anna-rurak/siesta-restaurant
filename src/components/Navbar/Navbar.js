6744c7e99599ddf1affa5cc770697ca264eb67bd
Anna <anna.rurakk@gmail.com>
2024-12-03 10:34:43 +0100

    updating navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -19,23 +18,18 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-white/70">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-12 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-italian-green hover:bg-italian-green/20 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <i  aria-hidden="true" className="fa-solid fa-bars block size-6 group-data-[open]:hidden"></i>
-              <i  aria-hidden="true" className="fa-solid fa-xmark hidden size-6 group-data-[open]:block"></i>
-            </DisclosureButton>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+
+            {/* Logo */}
+            {/* tutaj trzeba naprawić przeskakiwanie loga przy zmniejszaniu okna */}
             <div className="flex shrink-0 items-center">
-                {/* tutaj można wrzucić jakieś logo pizzy */}
-                <h1 className='italic text-italian-green text-2xl'>Siesta <i class="fa-solid fa-pizza-slice h-17 w-auto" alt="Siesta Restaurant"></i>
-                </h1>
+                <h1 className='italic text-italian-green text-2xl '>
+                    Siesta 
+               </h1>
             </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
+
+            {/* Navigation */}
+            <div className="hidden sm:flex flex-grow justify-center">
+              <div className="flex space-x-8">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
@@ -51,12 +45,21 @@ export default function Navbar() {
                 ))}
               </div>
             </div>
+
+         {/* Mobile menu button*/}
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            <DisclosureButton className="group relative align-right inline-flex items-center justify-center rounded-md p-2 text-italian-green hover:bg-italian-green/20 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <i  aria-hidden="true" className="fa-solid fa-bars block size-6 group-data-[open]:hidden"></i>
+              <i  aria-hidden="true" className="fa-solid fa-xmark hidden size-6 group-data-[open]:block"></i>
+            </DisclosureButton>
           </div>
         </div>
       </div>
 
       <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
+        <div className="flex flex-col space-y-2 px-4 pb-3 pt-2">
           {navigation.map((item) => (
             <DisclosureButton
               key={item.name}
@@ -64,7 +67,8 @@ export default function Navbar() {
               href={item.href}
               aria-current={item.current ? 'page' : undefined}
               className={classNames(
-                item.current ? 'bg-italian-green/50 text-white' : 'text-italian-green hover:bg-italian-green/20 hover:text-white',
+                item.current ? 'bg-italian-green/50 text-white' 
+                : 'text-italian-green hover:bg-italian-green/20 over:text-white',
                 'block rounded-md px-3 py-2 text-base font-medium',
               )}
             >
