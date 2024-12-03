40c13ba5568c1d133828ac83c4b824e4ed8b929d
Anna <anna.rurakk@gmail.com>
2024-12-03 08:44:05 +0100

    adding icons in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,25 +5,15 @@ export default function Footer(){
     <footer className="bg-white/70 bg-footer-texture py-6">
     <div className="flex justify-center gap-4">
         <a href="#">
-            <img
-                src="icon-facebook.png"
-                alt="Facebook"
-                className="w-8 h-8"
-            />
+            <i class="fa-brands fa-facebook w-10 h-10 text-white " alt="Facebook"></i>
         </a>
         <a href="#">
-            <img
-                src="icon-instagram.png"
-                alt="Instagram"
-                className="w-8 h-8"
-            />
+        <i class="fa-brands fa-twitter w-10 h-10 text-white" alt="Twitter"></i>
+
         </a>
         <a href="#">
-            <img
-                src="icon-twitter.png"
-                alt="Twitter"
-                className="w-8 h-8"
-            />
+        <i class="fa-brands fa-instagram w-10 h-10 text-white" alt="Instagram"></i>
+
         </a>
     </div>
 </footer>
