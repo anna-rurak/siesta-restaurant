0dc2b49088c222b214bdded8475b7a49959b3c3c
anna-rurak <anna.rurakk@gmail.com>
2024-12-22 21:30:57 +0100

    fixing some details in ordering subpage

--- src/Pages/OnlineOrders/OnlineOrders.js
+++ src/Pages/OnlineOrders/OnlineOrders.js
@@ -31,8 +31,8 @@ export default function OnlineOrders() {
 
             {/* ORDER SUMMARY */}
             <div className=" col-span-1 h-fit ">
-                <div className="my-8 mt-0 bg-italian-light-green overflow-hidden shadow-lg shadow-italian-green rounded-lg h-fit  ">
-                    <div className="w-full bg-italian-green py-6 text-left pl-12 ">
+                <div className="my-8 mt-0 bg-italian-light-green overflow-hidden shadow-lg shadow-italian-green  h-fit  ">
+                    <div className="w-full bg-italian-green py-6 text-left pl-12 rounded-lg ">
                         <h1 className=" text-white text-xl font-bold italic">
                             Twoje zamówienie
                         </h1>
@@ -54,8 +54,8 @@ export default function OnlineOrders() {
                 </div>
 
                 {/* PAYMENT METHODS */}
-                <div className="my-10 mt-0 bg-italian-light-green overflow-hidden shadow-lg shadow-italian-green rounded-lg h-fit  ">
-                    <div className="w-full bg-italian-green py-6 text-left pl-12 ">
+                <div className="my-10 mt-0 bg-italian-light-green overflow-hidden shadow-lg shadow-italian-green h-fit  ">
+                    <div className="w-full bg-italian-green py-6 text-left pl-12 rounded-lg ">
                         <h1 className=" text-white text-xl font-bold italic">
                             Metody płatności
                         </h1>
